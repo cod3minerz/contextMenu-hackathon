6faf8d69a2db30ec735a3bd612a905aab2e3d064
Hashpix <kirill.krivonos06@gmail.com>
2025-09-26 23:26:29 +0300

    add contextMenu code

--- src/menu.js
+++ src/menu.js
@@ -1,5 +1,43 @@
 import {Menu} from './core/menu'
 
 export class ContextMenu extends Menu {
+    constructor(selector) {
+        super(selector);
+        this.modules = [];
+        this.$el = document.querySelector(selector);
+
+        this.itemClicked = this.itemClicked.bind(this);
+        this.$el.addEventListener('click', this.itemClicked);
+    }
+
+    add(moduleItem) {
+        const duplicate = this.modules.find(m => m.type === moduleItem.type);
+        if (duplicate) return;
+        this.modules.push(moduleItem);
+        this.$el.innerHTML = this.modules.map(m => m.toHTML().join(''));
+    }
+
+    open(x, y) {
+        this.$el.style.display = 'block';
+        this.$el.style.left = `${x}px`;
+        this.$el.style.top = `${y}px`;
+    }
+
+    close() {
+        this.$el.style.display = 'none';
+    }
+
+    itemClicked(event) {
+        const liItem = event.target.closest('[data-type]');
+        if (!liItem) return;
+
+        const type = liItem.dataset.type;
+        const mod = this.modules.find(m => m.type === type);
+
+        if (mod) {
+            mod.trigger();
+        }
+        this.close();
+    }
 
 }
