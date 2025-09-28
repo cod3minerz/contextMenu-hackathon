a3d9850ed7394552d5839cfb8538f69d39deb0b4
Maks <maksimtambovcev2002@gmail.com>
2025-09-28 14:53:22 +0200

    Подключил модуль кликов в app.js

--- src/app.js
+++ src/app.js
@@ -1,40 +1,43 @@
-import './styles.css'
-import './dice.styles.css'
+import "./styles.css";
+import "./dice.styles.css";
 import { ContextMenu } from "./menu";
 import { ShapeModule } from "./modules/shape.module";
 import { AddTextModule } from "./modules/addText.module";
-import {SettingsModule} from "./modules/settings.module";
-import {DiceModule} from "./modules/dice.module";
-import {CursorModule} from "./modules/cursor.module";
-
+import { SettingsModule } from "./modules/settings.module";
+import { DiceModule } from "./modules/dice.module";
+import { CursorModule } from "./modules/cursor.module";
+import { BackgroundModule } from "./modules/background.module";
+import { ClicksModule } from "./modules/clicks.module";
 
 const createContainer = () => {
-    const container = document.createElement("div");
-    container.textContent = 'Зона контекстного меню'
-    container.classList.add("container");
-    document.body.append(container);
-}
+  const container = document.createElement("div");
+  container.textContent = "Зона контекстного меню";
+  container.classList.add("container");
+  document.body.append(container);
+};
 
-document.addEventListener('DOMContentLoaded', () => {
-    createContainer();
-    const menu = new ContextMenu('#menu');
-    const randomShape = new ShapeModule();
-    const text = new AddTextModule();
-    const settings = new SettingsModule();
-    const dice = new DiceModule();
-    const cursor = new CursorModule();
-    menu.add(randomShape);
-    menu.add(text);
-    menu.add(dice);
-    menu.add(cursor);
-    menu.add(settings);
+document.addEventListener("DOMContentLoaded", () => {
+  createContainer();
+  const menu = new ContextMenu("#menu");
+  const randomShape = new ShapeModule();
+  const text = new AddTextModule();
+  const settings = new SettingsModule();
+  const dice = new DiceModule();
+  const cursor = new CursorModule();
+  menu.add(randomShape);
+  menu.add(text);
+  menu.add(dice);
+  menu.add(cursor);
+  menu.add(settings);
+  menu.add(new BackgroundModule());
+  menu.add(new ClicksModule());
 
-    document.addEventListener('contextmenu', (event) => {
-        event.preventDefault();
-        if (event.target === document.querySelector('.container')) {
-            menu.open(event.clientX, event.clientY);
-        } else {
-            menu.close();
-        }
-    });
-})
+  document.addEventListener("contextmenu", (event) => {
+    event.preventDefault();
+    if (event.target === document.querySelector(".container")) {
+      menu.open(event.clientX, event.clientY);
+    } else {
+      menu.close();
+    }
+  });
+});
